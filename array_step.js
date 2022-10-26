ec7d82e97de5d120a7ad1d57c0512fa36ad71892
Anurag <anuragv83@gmail.com>
2022-10-26 20:16:19 +0530

    26th'Oct

---
+++ array_step.js
@@ -0,0 +1,24 @@
+function rangeBetween(start, end) 
+{
+  
+		if (start > end) {
+			const arr1 = new Array(start - end + 1);
+			for (let i = 0; i < arr1.length; i++, start--) {
+				resarrult[i] = start;
+			}
+			return arr1;
+		} 
+       else 
+       {
+			const arr2 = new Array(end-start+1);
+         
+		for (let j = 0; j < arr2.length; j++, start++) 
+        {
+			arr2[j] = start;
+		}
+      		return arr2;
+		}
+	}
+
+console.log(rangeBetwee(4, 7));
+console.log(rangeBetwee(-4, 7));
