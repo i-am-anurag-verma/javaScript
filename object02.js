d56892e3303f9437efffc032199ee8595c581628
Anurag <anuragv83@gmail.com>
2022-11-10 22:10:05 +0530

    10th'Nov

---
+++ object02.js
@@ -0,0 +1,25 @@
+var library = [
+    {
+        author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+    },
+    {
+        author: 'Ram',
+       title: 'XYZ',
+       readingStatus: true
+    },
+    {
+        author: 'Shyam',
+       title: 'ABC',
+       readingStatus: false
+    }
+]
+
+for(var data in library){
+    if(library[data].readingStatus==true){
+    console.log(`Already read  ${library[data].author} by the ${library[data].title}`);
+    }else{
+    console.log(`You still need to read  ${library[data].author} by ${library[data].title}`);
+    }
+    }
