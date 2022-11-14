b63f7c9ad6cd83688f6d7309c94983a980c62c17
Anurag <anuragv83@gmail.com>
2022-11-14 14:25:04 +0530

    14th'Nov

--- armstrong.js
+++ armstrong.js
@@ -1,14 +1,14 @@
-// program to check an Armstrong number of three digits
+//***************program to check an Armstrong number of three digits**********
 
 let sum = 0;
-const number = prompt('Enter a three-digit number - ');
+const number = 153
 
 let temp = number;
 while (temp > 0) {
-    let rem = temp % 10;
-
-    sum =sum + rem * rem * rem;
-    temp = parseInt(temp / 10);
+    
+    let remainder = temp % 10;
+    sum += remainder * remainder * remainder;
+    temp = parseInt(temp / 10); 
 }
 if (sum == number) {
     console.log(`${number} is an Armstrong number`);
