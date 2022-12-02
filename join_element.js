36ec53851da0a11ac0091d27014d1cc6bbd3d921
Anurag <anuragv83@gmail.com>
2022-12-02 15:46:00 +0530

    14:45

--- join_element.js
+++ join_element.js
@@ -1,4 +1,32 @@
-let myColor = ["Red", "Green", "Black", "White"];
-console.log(myColor.toString());
-console.log(myColor.join());
-console.log(myColor.join('+'));
+// let myColor = ["Red", "Green", "Black", "White"];
+// console.log(myColor.toString());
+// console.log(myColor.join());
+// console.log(myColor.join('+'));
+
+const number = [1, 2, 3, 4, 5, 6];
+
+const [one, two , ...rst] = number;
+
+console.log(one)
+console.log(two)
+console.log(rst)
+
+const a = number;
+
+number.pop();
+
+
+console.log(a, number)
+
+const b =[...number];
+
+b.pop();
+console.log(number, a, b)
+
+
+const arrValue = ['my', 'name' , 'is', 'Jack' ]
+
+console.log(arrValue);
+console.log(...arrValue);
+
+
