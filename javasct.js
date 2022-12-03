e97627c27c8f3f53e58d0d3e45d67262484d3425
Anurag <anuragv83@gmail.com>
2022-12-03 13:23:12 +0530

    myFun

--- javasct.js
+++ javasct.js
@@ -1,4 +1,11 @@
-function number(arr, input){
-
-
-}
+const myFun = function(array, input){
+    var result = [];
+    if(input === undefined) return array;
+    for (var i = 0; i < input; i++) {
+      result.push(array[i]);
+    } 
+    return result;
+   }
+  array = [1,2,3,4,5,6,7]
+   input = 5
+   console.log(myFun(array, input))
