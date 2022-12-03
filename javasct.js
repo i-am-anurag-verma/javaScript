44813b3777acf553e2724d3e9c95275133c44fbd
Anurag <anuragv83@gmail.com>
2022-12-03 23:33:40 +0530

    Split,parseInt

--- javasct.js
+++ javasct.js
@@ -1,11 +1,16 @@
-const myFun = function(array, input){
+const myFun = function (str, input) {
+
+    let arr = str.split(',')
+
     var result = [];
-    if(input === undefined) return array;
-    for (var i = 0; i < input; i++) {
-      result.push(array[i]);
-    } 
+
+    if (!input) return arr[0];
+    for (var i = 0; i < input; i++) 
+    {
+        result.push(parseInt(arr[i]));
+    }
     return result;
-   }
-  array = [1,2,3,4,5,6,7]
-   input = 5
-   console.log(myFun(array, input))
+}
+str = '1,2,3,4,5,6,7,8';
+input = 4
+console.log(myFun(str, input))
