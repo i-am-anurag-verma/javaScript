1feb53e2b564eccee4fbffd015cd6ee571a41260
Anurag <anuragv83@gmail.com>
2022-12-07 20:26:11 +0530

    20

---
+++ larger_num.js
@@ -0,0 +1,10 @@
+function larger_num(a, b){
+    if(a>b){
+        console.log(a, " is greater then ", b)
+    }else{
+        console.log(b, " is greater then ", a)
+    }
+}
+a = 20;
+b = 30;
+larger_num(a,b)
