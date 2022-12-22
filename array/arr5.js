53dab7287e8e5c077e0fcd926987a061df669e38
Anurag <anuragv83@gmail.com>
2022-12-22 23:26:59 +0530

    Thu

--- array/arr5.js
+++ array/arr5.js
@@ -1,21 +1,21 @@
-const arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
-let mf = 1;
-let m = 0;
+const arr = [1];
+let mf = 0;
+let count = 0;
 let item;
-for (let i=0; i<arr1.length; i++)
-{
-        for (let j=i; j<arr1.length; j++)
-        {
-                if (arr1[i] == arr1[j])
-                 m++;
-                if (mf<m)
-                {
-                  mf=m; 
-                  item = arr1[i];
+if (arr.length) {
+        for (let i = 0; i < arr.length; i++) {
+                for (let j = i; j < arr.length; j++) {
+                        if (arr[i] == arr[j]) {
+                                count++;
+                        }
+                        if (mf < count) {
+                                mf = count;
+                                item = arr[i];
+                        }
                 }
-
+                count = 0;
         }
-        m=0;
-
+        console.log(`${item} ( ${mf} times )`);
+} else {
+        console.log('empty')
 }
-console.log(`${item} ( ${mf} times ) `) ;
--- array/fibonacci.js
+++ array/fibonacci.js
@@ -1,15 +1,12 @@
-function fibonacci(num){
 
-    let num1 = 0;
-    let num2 = 1;
-    let nextTerm;
+let num = 8
+let num1 = 0;
+let num2 = 1;
+let nextTerm;
 
-    for(let i = 0; i < num; i++){
-        nextTerm = num1 + num2;
-        num1 = num2;
-        num2 = nextTerm
-    }
-    return num2
+for (let i = 0; i < num; i++) {
+    console.log(num1)
+    nextTerm = num1 + num2;
+    num1 = num2;
+    num2 = nextTerm;
 }
-
-console.log(fibonacci(8))
