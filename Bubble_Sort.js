2b1fa7c138b231245d7a83d9134bf5b196d12636
i-am-anurag-verma <anuragv83@gmail.com>
2023-01-11 17:54:40 +0530

    11th'jan

--- Bubble_Sort.js
+++ Bubble_Sort.js
@@ -1,8 +1,8 @@
 function BubbleSort(arr){
     let temp;
-    for(let i = 0; i<arr.length-1; i++){
+    for(let i = 0; i<arr.length; i++){
 
-        for(let j = 0; j<arr.length-1; j++){
+        for(let j = 0; j<arr.length; j++){
 
             if(arr[j]>arr[j+1]){
 
